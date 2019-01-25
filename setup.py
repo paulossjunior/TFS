15a1d5bfdb6e5fc8c2f2f916949825f4248099b5
Paulo Sérgio dos Santos Júnior <paulossjunior@gmail.com>
2019-01-25 20:49:48 -0200

    Criando o pacote de configuração

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='TFS',  # Required
+
+    version='0.0.1',  # Required
+    packages=['RabbitMQ'],
+    #packages=setuptools.find_packages(),
+
+    install_requires=['vsts'],  # Optional
+
+    
+)
