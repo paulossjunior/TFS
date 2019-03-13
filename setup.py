151c62f2cf9fcb7ab79a4ef9cd9f11eac29e91fa
Paulo Sérgio dos Santos Júnior <paulossjunior@gmail.com>
2019-03-13 14:48:06 -0300

    criando as configuração para o pip

--- setup.py
+++ setup.py
@@ -2,10 +2,17 @@ from setuptools import setup, find_packages
 
 setup(
     name='TFS',  # Required
-
     version='0.0.4',  # Required
-    packages=['TFS'],
-    install_requires=['vsts'],  # Optional
-
-    
+    author="Paulo Sergio dos Santo Junior",
+    author_email="paulossjuniort@gmail.com",
+    description="Wrapper para acessar as informações do TFS",
+    url="https://github.com/paulossjunior/tfs",
+    packages=find_packages(),
+    install_requires=['vsts','requestx'], 
+    classifiers=[
+         "Programming Language :: Python :: 3",
+         "License :: OSI Approved :: MIT License",
+         "Operating System :: OS Independent",
+     ],
+    setup_requires=['wheel'],
 )
